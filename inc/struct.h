4318059b271bed4bd54158e4df2767eceac22cd2
Maximiliano Velazquez <mvelazqu@cbr1s1.42barcelona.com>
2025-06-15 16:45:01 +0200

    Fresh start

--- inc/struct.h
+++ inc/struct.h
@@ -10,7 +10,43 @@ struct s_env
 	char	*key;
 	char	*value;
 	t_env	*next;
-}
+};
+
+typedef struct s_shell
+{
+	char	**envp;//Ah que quitarlo
+	t_env	*env;
+	int		status;
+	int		finished;
+}	t_shell;
+
+typedef struct s_token	t_token;
+
+struct s_token
+{
+	char	*word;
+	int		type;
+	t_token	*next;
+};
+
+typedef struct s_file	t_file;
+
+struct s_file
+{
+	char	*file_name;
+	int		type;
+	t_file	*next;
+};
+
+typedef struct s_cmd	t_cmd;
+
+struct s_cmd
+{
+	char	*path;
+	char	**argv;
+	t_file	*files;
+	t_cmd	*next;
+};
 
 void	delete_env(t_env *env);
 void	print_env(t_env *env);
